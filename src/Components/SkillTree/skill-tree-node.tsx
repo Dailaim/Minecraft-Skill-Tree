d5960c523a8f83f1004baedb11d2da015644da4a
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-10-09 15:54:28 -0500

    feat: Refactor SkillTreeNode component to enhance rendering and tooltip functionality

--- src/Components/SkillTree/skill-tree-node.tsx
+++ src/Components/SkillTree/skill-tree-node.tsx
@@ -1,126 +1,110 @@
 "use client";
 
-import { unlockNode } from "../../features/skill-tree-slice";
-import { useAppDispatch } from "../../hooks";
+import { useState } from "react";
 import { cn } from "../../utils";
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
 } from "../ui/tooltip";
+import { ButtonSkillTreeNode } from "./button-skill-tree-node";
 
 interface SkillNode {
-  id: string;
-  name: string;
-  description: string;
-  image: string;
-  parentId: string | null;
+	id: string;
+	name: string;
+	description: string;
+	image: string;
+	parentId: string | null;
 }
 
 interface SkillTreeNodeProps {
-  node: SkillNode;
-  unlocked?: boolean;
-  nodeId: string;
-  canUnlock?: boolean;
+	node: SkillNode;
+	unlocked?: boolean;
+	nodeId: string;
+	canUnlock?: boolean;
+	position: {
+		y: number;
+		x: number;
+	};
+	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export function SkillTreeNode({
-  node,
-  unlocked = false,
-  nodeId,
-  canUnlock = true,
+	node,
+	unlocked = false,
+	onClick,
+	canUnlock = true,
+	position,
 }: SkillTreeNodeProps) {
-  const dispatch = useAppDispatch();
+	const [isHover, changeHover] = useState(false);
 
-  const handleClick = () => {
-    if (!unlocked && canUnlock) {
-      dispatch(unlockNode(nodeId));
-    }
-  };
-
-  const isDisabled = !unlocked && !canUnlock;
-
-  return (
-    <TooltipProvider delayDuration={200}>
-      <Tooltip >
-        <TooltipTrigger asChild>
-          <button
-            type="button"
-            onClick={handleClick}
-            disabled={isDisabled}
-            className={
-              cn("w-16 h-16 transition-all duration-300 pixel-corners p-0 m-0 border-2 border-black",
-              )
-            }
-            aria-label={node.name}
-          >
-            <div className={cn("w-full h-full overflow-hidden border-3 shadow shadow-black ",
-
-              unlocked
-                ? // 🔸 Estado desbloqueado (naranja)
-                `bg-[#aa7e0f]
-       border-t-[color-mix(in_srgb,_#aa7e0f_70%,_white_30%)]
-       border-l-[color-mix(in_srgb,_#aa7e0f_70%,_white_30%)]
-       border-b-[color-mix(in_srgb,_#aa7e0f_70%,_black_50%)]
-       border-r-[color-mix(in_srgb,_#aa7e0f_70%,_black_50%)]
-       shadow-[#aa7e0f]/30`
-                : // 🔹 Estado bloqueado (gris/azul)
-                `bg-[#c6c6c6]
-       border-t-[color-mix(in_srgb,_#c6c6c6_70%,_white_30%)]
-       border-l-[color-mix(in_srgb,_#c6c6c6_70%,_white_30%)]
-       border-b-[color-mix(in_srgb,_#c6c6c6_70%,_black_50%)]
-       border-r-[color-mix(in_srgb,_#c6c6c6_70%,_black_50%)]
-       shadow-[#006c9a]/20`
-            )}>
-              <img
-                src={node.image || "/placeholder.svg"}
-                alt={node.name}
-                className="pixelated p-1 object-contain w-full h-full max-w-full max-h-full"
-              />
-            </div>
-
-          </button>
-        </TooltipTrigger>
-        <TooltipContent
-          side="bottom"
-          sideOffset={8}
-          className="relative max-w-xs p-0 border-0 bg-transparent ring-0 pixel-corners-wrapper shadow-none"
-        >
-          <div className="relative pixel-corners">
-            {/* Título con esquinas recortadas */}
-            <h3
-              className={cn(
-                "relative font-minecraft text-white text-sm px-3 py-2 border-3",
-                unlocked
-                  ? // 🔸 Estado naranja
-                  `bg-[#ac7c0c]
+	return (
+		<TooltipProvider delayDuration={0}>
+			<Tooltip delayDuration={0} open={isHover} onOpenChange={changeHover}>
+				<TooltipTrigger asChild>
+					<ButtonSkillTreeNode
+						node={node}
+						nodeId={node.id}
+						position={position}
+						unlocked={unlocked}
+						onClick={onClick}
+						canUnlock={canUnlock}
+						isHover={isHover}
+					/>
+				</TooltipTrigger>
+				<TooltipContent
+					side="right"
+					alignOffset={6}
+					align="start"
+					sideOffset={-80}
+					className="p-0 group border-0 bg-transparent ring-0 pixel-corners-wrapper shadow-none z-[9998]"
+				>
+					<div className="relative pixel-corners">
+						<div className="absolute  group-data-[side=left]:right-[13px] top-[-9px] group-data-[side=right]:left-[13px] z-[99999]">
+							<ButtonSkillTreeNode
+								node={node}
+								nodeId={node.id}
+								position={position}
+								unlocked={unlocked}
+								onClick={onClick}
+								canUnlock={canUnlock}
+								isHover={isHover}
+							/>
+						</div>
+						<div
+							className={cn(
+								"relative font-minecraft text-white text-xl pe-2 py-2  group-data-[side=right]:ps-20 group-data-[side=left]:ps-2 group-data-[side=left]:pe-20",
+								"border-3",
+								unlocked
+									? `bg-[#ac7c0c]
        border-t-[color-mix(in_srgb,_#ac7c0c_70%,_white_30%)]
        border-l-[color-mix(in_srgb,_#ac7c0c_70%,_white_30%)]
        border-b-[color-mix(in_srgb,_#ac7c0c_70%,_black_40%)]
        border-r-[color-mix(in_srgb,_#ac7c0c_70%,_black_40%)]`
-                  : // 🔹 Estado azul
-                  `bg-[#006c9a]
+									: // 🔹 Estado azul
+										`bg-[#006c9a]
        border-t-[color-mix(in_srgb,_#006c9a_70%,_white_30%)]
        border-l-[color-mix(in_srgb,_#006c9a_70%,_white_30%)]
        border-b-[color-mix(in_srgb,_#006c9a_70%,_black_40%)]
-       border-r-[color-mix(in_srgb,_#006c9a_70%,_black_40%)]`
-              )}
-            >
-              {node.name}
+       border-r-[color-mix(in_srgb,_#006c9a_70%,_black_40%)]`,
+								"",
+							)}
+						>
+							{node.name}
+						</div>
 
-
-            </h3>
-
-            {/* Descripción */}
-            <p className=" bg-[#212121] text-[#00ff26] text-[12px] font-minecraft px-3 py-3 leading-snug border-3 border-t-black border-[color-mix(in_srgb,_#474747_70%,_white_20%)]">
-              {node.description}
-
-            </p>
-          </div>
-
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
+						<div
+							className="w-full text-[#00ff26] bg-[#212121] h-full border-3 border-t-black
+						border-[color-mix(in_srgb,_#474747_70%,_white_20%)] "
+						>
+							<p className="max-w-md text-lg font-minecraft px-3 pt-4 pb-3 leading-snug ">
+								{node.description}
+							</p>
+						</div>
+					</div>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
 }
